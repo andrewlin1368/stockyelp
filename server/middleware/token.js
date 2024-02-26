59d0bf451d61c3ede32c7cd1bd51fe4539fb8111
Andrew <andrewlin1368@gmail.com>
2024-02-26 13:34:28 -0500

    middleware

---
+++ server/middleware/token.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const auth = req.headers.authorization;
+  const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
+
+  try {
+    req.user = jwt.verify(token, process.env.JWT);
+  } catch {
+    req.user = null;
+  }
+  next();
+};
+
+module.exports = { verifyToken };
