65658095857945aada44b6af56ae65c19cb4c518
Andrew <andrewlin1368@gmail.com>
2024-02-26 17:50:00 -0500

    updated router

--- server/api/stocks.js
+++ server/api/stocks.js
@@ -7,6 +7,8 @@ const {
   unfollow,
   upvote,
   downvote,
+  removeComment,
+  createComment,
 } = require("../../db/index.js");
 const { verifyToken } = require("../middleware/token.js");
 
@@ -84,6 +86,37 @@ stockRouter.post("/downvote", verifyToken, async (req, res, next) => {
     return data.error
       ? res.status(400).send({ error: data.error })
       : res.send(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//remove a comment
+stockRouter.put("/removecomment", verifyToken, async (req, res, next) => {
+  try {
+    if (!req.user)
+      return res.status(400).send({ error: "Invalid credentials" });
+    const { comment_id } = req.body;
+    const data = await removeComment({ comment_id, user_id: req.user.user_id });
+    return data.error
+      ? res.status(400).send({ error: data.error })
+      : res.send(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//create comment
+stockRouter.post("/addcomment", verifyToken, async (req, res, next) => {
+  try {
+    if (!req.user)
+      return res.status(400).send({ error: "Invalid credentials" });
+    const { stock_id, message } = req.body;
+    if (message.length === 0)
+      return res.status(400).send({ error: "Cannot post empty messages" });
+    return res.send(
+      await createComment({ stock_id, user_id: req.user.user_id, message })
+    );
   } catch (error) {
     next(error);
   }
