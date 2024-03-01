12b6a54c3f7432defd963f884c85601dfff6941a
Andrew <andrewlin1368@gmail.com>
2024-03-01 17:50:01 -0500

    added skeleton navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,18 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./api/store.js";
 import Navbarcomponent from "./components/Navbar.jsx";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Navbarcomponent></Navbarcomponent>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Navbarcomponent></Navbarcomponent>
+        <Routes>
+          <Route path="/" element={<App></App>}></Route>
+          <Route path="*" element={<Navigate to="/" />}></Route>
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
