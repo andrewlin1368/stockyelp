c915771386352228fa47a269187d32e6aa6695c2
Andrew <andrewlin1368@gmail.com>
2024-03-02 21:10:08 -0500

    updated routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import { useGetAllStocksQuery } from "./api/stocksApi";
 import Stocks from "./components/Stocks";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Profile from "./components/Profile.jsx";
+import Navbarcomponent from "./components/Navbar.jsx";
 
 function App() {
   const { isLoading } = useGetAllStocksQuery();
-  return <>{(isLoading && <>Loading...</>) || <Stocks></Stocks>}</>;
+  // return <>{(isLoading && <>Loading...</>) || <Stocks></Stocks>}</>;
+  return (
+    <BrowserRouter>
+      <Navbarcomponent></Navbarcomponent>
+      <Routes>
+        <Route path="/" element={<Stocks></Stocks>}></Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,19 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./api/store.js";
-import Navbarcomponent from "./components/Navbar.jsx";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Navbarcomponent></Navbarcomponent>
-        <Routes>
-          <Route path="/" element={<App></App>}></Route>
-          <Route path="*" element={<Navigate to="/" />}></Route>
-        </Routes>
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App></App>
+    </Provider>
   </React.StrictMode>
 );
