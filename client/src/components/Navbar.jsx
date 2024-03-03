f98ba63789ef4ccb51731c90af07dac4fb379516
Andrew <andrewlin1368@gmail.com>
2024-03-02 21:10:40 -0500

    updated navbar to display data based on token

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,34 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useSelector } from "react-redux";
+import { logoutUser } from "../api/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Navbarcomponent() {
+  const { token } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch(logoutUser());
+    navigate("/");
+  };
   return (
     <>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">StockYelp</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Stocks</Nav.Link>
-            <Nav.Link href="#features">Login</Nav.Link>
-            <Nav.Link href="#pricing">Register</Nav.Link>
-            <Nav.Link href="#pricing">Profile</Nav.Link>
+          <Navbar.Brand href="/" className="lead">
+            StockYelp
+          </Navbar.Brand>
+          <Nav className="me-auto lead">
+            {!token && <Nav.Link href="/login">Login</Nav.Link>}
+            {!token && <Nav.Link href="/register">Register</Nav.Link>}
+            {token && <Nav.Link href="/profile">Profile</Nav.Link>}
+            {token && (
+              <Nav.Link href="" onClick={logout}>
+                Logout
+              </Nav.Link>
+            )}
           </Nav>
         </Container>
       </Navbar>
