e5026b3af68324405aca45ef336190e7f4b362df
Andrew <andrewlin1368@gmail.com>
2024-03-02 21:37:43 -0500

    added logos

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,15 +18,27 @@ function Navbarcomponent() {
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="/" className="lead">
-            StockYelp
+            <i class="bi bi-coin"></i> StockYelp
           </Navbar.Brand>
           <Nav className="me-auto lead">
-            {!token && <Nav.Link href="/login">Login</Nav.Link>}
-            {!token && <Nav.Link href="/register">Register</Nav.Link>}
-            {token && <Nav.Link href="/profile">Profile</Nav.Link>}
+            {!token && (
+              <Nav.Link href="/login">
+                Login <i class="bi bi-box-arrow-in-right"></i>
+              </Nav.Link>
+            )}
+            {!token && (
+              <Nav.Link href="/register">
+                Register <i class="bi bi-person-plus-fill"></i>
+              </Nav.Link>
+            )}
+            {token && (
+              <Nav.Link href="/profile">
+                Profile <i class="bi bi-person-circle"></i>
+              </Nav.Link>
+            )}
             {token && (
               <Nav.Link href="" onClick={logout}>
-                Logout
+                Logout <i class="bi bi-box-arrow-right"></i>
               </Nav.Link>
             )}
           </Nav>
