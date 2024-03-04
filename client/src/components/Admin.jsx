2ac6c8ec6cee0a5ccfaabb6368bf96fc08c97b9c
Andrew <andrewlin1368@gmail.com>
2024-03-04 16:57:36 -0500

    redirect if not admin and fix some issues

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -7,9 +7,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useAddstockMutation } from "../api/stocksApi";
+import { useNavigate } from "react-router-dom";
 
 export default function Admin() {
   const [addStock] = useAddstockMutation();
+  const navigate = useNavigate();
   const [form, setForm] = useState({
     fullname: "",
     symbol: "",
@@ -107,98 +109,107 @@ export default function Admin() {
   const updateForm = (e) =>
     setForm({ ...form, [e.target.name]: e.target.value });
   const { user } = useSelector((state) => state.user);
+  useEffect(() => {
+    const valid = () => {
+      navigate("/");
+    };
+    if (!user || (user && !user.isadmin)) valid();
+  }, []);
   return (
-    <div>
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <h1 className="display-6">Add a new stock</h1>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="form-group mt-1 mb-3">
-            <input
-              type="text"
-              className="form-control"
-              name="fullname"
-              placeholder="Name"
-              onChange={(e) => updateForm(e)}
-            />
-          </div>
-          <div className="form-group mt-3 mb-3">
-            <input
-              type="text"
-              className="form-control"
-              name="symbol"
-              placeholder="Symbol"
-              onChange={(e) => updateForm(e)}
-            />
-          </div>
-          <div className="form-group mt-3 mb-3">
-            <textarea
-              className="form-control"
-              name="description"
-              placeholder="Description"
-              onChange={(e) => updateForm(e)}
-            />
-          </div>
-          <div className="input-group mt-3 mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">$</span>
+    user &&
+    user.isadmin && (
+      <div>
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <h1 className="display-6">Add a new stock</h1>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="form-group mt-1 mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="fullname"
+                placeholder="Name"
+                onChange={(e) => updateForm(e)}
+              />
             </div>
-            <input
-              type="number"
-              className="form-control"
-              name="price"
-              placeholder="Current Price"
-              onChange={(e) => updateForm(e)}
-            />
-          </div>
-          <div className="input-group mt-3 mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">$</span>
+            <div className="form-group mt-3 mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="symbol"
+                placeholder="Symbol"
+                onChange={(e) => updateForm(e)}
+              />
             </div>
-            <input
-              type="number"
-              className="form-control"
-              name="week_low"
-              placeholder="52 Week Low"
-              onChange={(e) => updateForm(e)}
-            />
-          </div>
-          <div className="input-group mt-3 mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">$</span>
+            <div className="form-group mt-3 mb-3">
+              <textarea
+                className="form-control"
+                name="description"
+                placeholder="Description"
+                onChange={(e) => updateForm(e)}
+              />
             </div>
-            <input
-              type="number"
-              className="form-control"
-              name="week_high"
-              placeholder="52 Week High"
-              onChange={(e) => updateForm(e)}
-            />
+            <div className="input-group mt-3 mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">$</span>
+              </div>
+              <input
+                type="number"
+                className="form-control"
+                name="price"
+                placeholder="Current Price"
+                onChange={(e) => updateForm(e)}
+              />
+            </div>
+            <div className="input-group mt-3 mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">$</span>
+              </div>
+              <input
+                type="number"
+                className="form-control"
+                name="week_low"
+                placeholder="52 Week Low"
+                onChange={(e) => updateForm(e)}
+              />
+            </div>
+            <div className="input-group mt-3 mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">$</span>
+              </div>
+              <input
+                type="number"
+                className="form-control"
+                name="week_high"
+                placeholder="52 Week High"
+                onChange={(e) => updateForm(e)}
+              />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={addNewStock}>
+              Add
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <div className="topsection">
+          <h1 className="display-6 mt-3 mb-3" style={{ textAlign: "center" }}>
+            Welcome @{user.username}!
+          </h1>
+          <div className="lead" style={{ textAlign: "center" }}>
+            Need to add a new stock?{" "}
+            <Link style={{ textDecoration: "none" }} onClick={handleShow}>
+              Click me!
+            </Link>{" "}
+            or to edit an existing stock?{" "}
+            <Link style={{ textDecoration: "none" }}>Click me!</Link>
           </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={addNewStock}>
-            Add
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <div className="topsection">
-        <h1 className="display-6 mt-3 mb-3" style={{ textAlign: "center" }}>
-          Welcome @{user.username}!
-        </h1>
-        <div className="lead" style={{ textAlign: "center" }}>
-          Need to add a new stock?{" "}
-          <Link style={{ textDecoration: "none" }} onClick={handleShow}>
-            Click me!
-          </Link>{" "}
-          or to edit an existing stock?{" "}
-          <Link style={{ textDecoration: "none" }}>Click me!</Link>
         </div>
+        <ToastContainer></ToastContainer>
       </div>
-      <ToastContainer></ToastContainer>
-    </div>
+    )
   );
 }
